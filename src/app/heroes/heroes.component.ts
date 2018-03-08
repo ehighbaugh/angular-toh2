399ad532cfbe49007ad546ff6e3d5d232e6d34c4
Erin Highbaugh <erinhighbaugh@gmail.com>
2018-03-08 09:55:41 -0500

    moved hero-detail to separate component

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -7,16 +7,21 @@ import { HEROES } from '../mock-heroes';
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
+
 export class HeroesComponent implements OnInit {
-  hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
+
   heroes = HEROES;
+
+  selectedHero: Hero;
 
   constructor() { }
 
   ngOnInit() {
   }
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+
 
 }
